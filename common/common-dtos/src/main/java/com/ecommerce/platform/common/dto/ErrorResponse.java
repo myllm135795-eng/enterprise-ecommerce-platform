d169c61344cdf22499bb1b86f22c9b14c8abdbae
myllm135795-eng <myllm135795@gmail.com>
2026-07-16 13:08:23 +0530

    Add ErrorResponse DTO

---
+++ common/common-dtos/src/main/java/com/ecommerce/platform/common/dto/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.ecommerce.platform.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private String errorCode;
+    private String timestamp;
+    private String path;
+}

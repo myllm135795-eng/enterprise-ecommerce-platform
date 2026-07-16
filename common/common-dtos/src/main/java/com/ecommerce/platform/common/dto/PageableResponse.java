4f587e62be4738f0505223f48c3e60eab7b5a3ef
myllm135795-eng <myllm135795@gmail.com>
2026-07-16 13:08:13 +0530

    Add PageableResponse DTO

---
+++ common/common-dtos/src/main/java/com/ecommerce/platform/common/dto/PageableResponse.java
@@ -0,0 +1,20 @@
+package com.ecommerce.platform.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PageableResponse<T> {
+    private java.util.List<T> content;
+    private int pageNumber;
+    private int pageSize;
+    private long totalElements;
+    private int totalPages;
+    private boolean hasNext;
+    private boolean hasPrevious;
+}
